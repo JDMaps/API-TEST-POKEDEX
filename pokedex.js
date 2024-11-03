f690384f2721626f77cf3bb68248c7bece0dc732
JDMaps <jcmapa28@gmail.com>
2024-11-03 23:10:31 +0800

    changed to H sports sampler

--- pokedex.js
+++ pokedex.js
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
-fetch("https://pokeapi.co/api/v2/pokemon?limit=151%27")
-    .then(function(pokedex)
+fetch("https://hpplussport.com/api/products")
+    .then(function(response)
     {
-return pokedex.json();
+return response.json();
 
     })
-    .then(function(pokedexEntry)
+    .then(function(jsonData)
     {
-       console.log(pokedexEntry);
-=======
-fetch("https://pokeapi.co/api/v2/pokemon?limit=151%27")
-    .then(function(pokedex)
-    {
-return pokedex.json();
-
-    })
-    .then(function(pokedexEntry)
-    {
-       console.log(pokedexEntry);
->>>>>>> bb5664aa0474de995f7cc20cb783ef8fec1f6cd4
+       console.log(jsonData);
+       
+       
     })
